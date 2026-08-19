2e1794046d4d6f412ad0517e6017885f802bc346
Kokomolokolo <kokoloko.gaming@gmail.com>
2026-08-19 12:55:41 +0200

    update to player movement

--- src/player/movement.rs
+++ src/player/movement.rs
@@ -1,15 +1,16 @@
 use avian3d::dynamics::rigid_body::LinearVelocity;
-use bevy::prelude::*;
+use bevy::{gizmos::transform_gizmo, prelude::*};
 
 use crate::player::Player;
 
 pub fn player_movement(
     keyboard: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut LinearVelocity, With<Player>>,
+    mut query: Query<(&mut LinearVelocity, &mut Transform), With<Player>>,
+    time: Res<Time>
 ) {
     const SPEED: f32 = 2.0;
 
-    for mut velocity in &mut query {
+    for (mut velocity, mut transform) in &mut query {
         let mut direction = Vec3::ZERO;
 
         if keyboard.pressed(KeyCode::KeyW) {
@@ -26,6 +27,11 @@ pub fn player_movement(
         }
 
         if direction.length_squared() > 0.0 {
+            // Blickrichtung des Spielers in die Bewegungsrichtug
+            let target_angle = direction.x.atan2(direction.z);
+            let target_rotation = Quat::from_rotation_y(target_angle);
+            transform.rotation = transform.rotation.slerp(target_rotation, 10.0 * time.delta_secs());
+            
             direction = direction.normalize();
         }
 
